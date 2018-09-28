a6d07f0047ea9f4ea7693d8ab3c225204b7056bd
Marius Brad <mbrad26@gmail.com>
2018-09-28 11:41:36 +0100

    OK

--- game_functions.py
+++ game_functions.py
@@ -42,7 +42,7 @@ def create_fleet(ai_settings, screen, ship, aliens):
     number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
     number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
     for row_number in range(number_rows):
-        for alien_number in  range(number_aliens_x):
+        for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
 
 
